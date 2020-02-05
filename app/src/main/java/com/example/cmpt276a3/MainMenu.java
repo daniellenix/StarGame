a46381eb2ea7a876fae5b12a01c31e051f81f855
Danielle <dnix@sfu.ca>
2020-02-04 18:31:43 -0800

    Design finished.

--- app/src/main/java/com/example/cmpt276a3/MainMenu.java
+++ app/src/main/java/com/example/cmpt276a3/MainMenu.java
@@ -2,7 +2,11 @@ package com.example.cmpt276a3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        
+        playGame();
+        options();
+        help();
     }
+
+    private void playGame() {
+        Button btn = findViewById(R.id.buttonPlayGame);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = ThirdActivity.makeIntent(MainActivity.this, position);
+//                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    private void options() {
+        Button btn = findViewById(R.id.buttonOptions);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = ThirdActivity.makeIntent(MainActivity.this, position);
+//                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    private void help() {
+        Button btn = findViewById(R.id.buttonHelp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = ThirdActivity.makeIntent(MainActivity.this, position);
+//                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+
+
 }
