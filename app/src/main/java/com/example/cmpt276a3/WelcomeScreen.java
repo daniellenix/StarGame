deb1ad846a40c633f8f7f6aea97e641611f79752
mahekkhan <mahekk@sfu.ca>
2020-02-06 15:07:43 -0800

    February 6, 2020

--- app/src/main/java/com/example/cmpt276a3/WelcomeScreen.java
+++ app/src/main/java/com/example/cmpt276a3/WelcomeScreen.java
@@ -1,5 +1,9 @@
 package com.example.cmpt276a3;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -9,12 +13,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity{
+
+    Animation rotateAnimation;
+    TextView text_animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
+        text_animation = findViewById(R.id.welcome_Title);
+
+        //overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
         skip();
+
     }
 
     private void skip() {
@@ -26,6 +38,11 @@ public class WelcomeScreen extends AppCompatActivity{
 //                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    private void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+        text_animation.startAnimation(animation);
     }
 
 }
