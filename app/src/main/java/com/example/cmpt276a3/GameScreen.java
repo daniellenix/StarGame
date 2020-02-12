4e0278119597619d3c1d6027b9f1549204afdca3
Danielle <dnix@sfu.ca>
2020-02-12 11:42:05 -0800

    Game board displays, star icons shown when clicked.

--- app/src/main/java/com/example/cmpt276a3/GameScreen.java
+++ app/src/main/java/com/example/cmpt276a3/GameScreen.java
@@ -1,12 +1,29 @@
 package com.example.cmpt276a3;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 public class GameScreen extends AppCompatActivity {
+
+    private static final int NUM_ROWS = 7;
+    private static final int NUM_COLS = 10;
+
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
 
     public static Intent makeIntent(Context context) {
         Intent intent =  new Intent(context, GameScreen.class);
@@ -17,5 +34,98 @@ public class GameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = findViewById(R.id.tableForButtons);
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+            table.addView(tableRow);
+
+            for (int col = 0; col < NUM_COLS; col++){
+                final int FINAL_COL = col;
+                final int FINAL_ROW = row;
+
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                button.setText("" + col + "," + row);
+
+                // Make text not clip on small buttons
+                button.setPadding(0, 0, 0, 0);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked(FINAL_COL, FINAL_ROW);
+                    }
+                });
+
+                tableRow.addView(button);
+                buttons[row][col] = button;
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void gridButtonClicked(int col, int row) {
+        Toast.makeText(this, "Button clicked: " + col + "," + row,
+                Toast.LENGTH_SHORT).show();
+        Button button = buttons[row][col];
+
+        // Lock Button Sizes:
+        lockButtonSizes();
+
+        // Does not scale image.
+//    	button.setBackgroundResource(R.drawable.action_lock_pink);
+
+        // Scale image to button: Only works in JellyBean!
+        // Image from Crystal Clear icon set, under LGPL
+        // http://commons.wikimedia.org/wiki/Crystal_Clear
+        int newWidth = button.getWidth();
+        int newHeight = button.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.star);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+
+        // Change text on button:
+        button.setText("" + col);
+
+    }
+
+    private void lockButtonSizes() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                Button button = buttons[row][col];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
