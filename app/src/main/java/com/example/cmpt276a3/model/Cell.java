95c11a0128265b87f95dd1105b55feddffd426a2
Danielle <dnix@sfu.ca>
2020-02-13 12:40:44 -0800

    cell changed

--- app/src/main/java/com/example/cmpt276a3/model/Cell.java
+++ app/src/main/java/com/example/cmpt276a3/model/Cell.java
@@ -3,50 +3,35 @@ package com.example.cmpt276a3.model;
 public class Cell {
     private int row;
     private int column;
-    private int stars[][];
-    private boolean exploredCell[][];
-    private boolean isStars[][];
+    private boolean hasStar;
 
-    Cell(int row, int column){
-        this.row = 0;
-        this.column = 0;
-        stars[row][column]=0;
+    public Cell(int row, int column, boolean hasStar) {
+        this.row = row;
+        this.column = column;
+        this.hasStar = hasStar;
     }
 
-    public int getRow(){
+    public int getRow() {
         return row;
     }
 
-    public int getColumn(){
-        return column;
-    }
-
-
     public void setRow(int row) {
         this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
     }
 
     public void setColumn(int column) {
         this.column = column;
     }
 
-    //checks if star is in particular cell
-    public boolean isStars(int row, int column){
-        return isStars[row][column];
+    public boolean isHasStar() {
+        return hasStar;
     }
 
-    //checks if cell already explored
-    public boolean getExploredCells(int row, int column){
-        return exploredCell[row][column];
-
+    public void setHasStar(boolean hasStar) {
+        this.hasStar = hasStar;
     }
-
-
-    public void e(int row, int column){
-        exploredCell[row][column] = true;
-        if(isStars(row,column)){
-            stars[row][column] = stars[row-1][column-1];
-        }
-    }
-
 }
